f6c6cdf89d9bfd16c269095b882a4ee33bc164a4
Chris Hogan <hogancm@gmail.com>
2017-02-16 15:27:25 -0500

    Updated packages

---
+++ Buddy.AspNetCore46/Web/RequestLogger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
+
+namespace Buddy.AspNetCore.Web
+{
+    public static class RequestLogger
+    {
+        public const string LogFormat = "{0} | {1} {2}";
+
+        public static IApplicationBuilder UseSimpleLogger(this IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            var logger = loggerFactory.CreateLogger(nameof(RequestLogger));
+
+            app.Use(async (context, next) =>
+            {
+                await next();
+                
+                var status = context.Response.StatusCode;
+                var method = context.Request.Method;
+                var path = context.Request.Path + WebUtility.UrlDecode(context.Request.QueryString.Value);
+
+                if (status < 400)
+                    logger.LogInformation(LogFormat, status, method, path);
+                else if (status < 500)
+                    logger.LogWarning(LogFormat, status, method, path);
+                else
+                    logger.LogError(LogFormat, status, method, path);
+            });
+
+            return app;
+        }
+    }
+}
---
+++ Buddy.AspNetCore46/Web/Service/UrlQueryModelBinder.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Buddy.Web.UrlQuery;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Buddy.AspNetCore.Web.Service
+{
+    public class UrlQueryModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var queryString = WebUtility.UrlDecode(bindingContext.ActionContext.HttpContext.Request.QueryString.Value.TrimStart('?'));
+            bindingContext.Result = ModelBindingResult.Success(queryString.ToQueryObject(bindingContext.ModelType));
+            return Task.CompletedTask;
+        }
+    }
+}
